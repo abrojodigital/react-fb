8915156eb1c46eafafc052970b1e18a74fddc341
abrojodigital <abrojodigital@gmail.com>
2022-07-25 18:08:17 -0300

    Auth y login

--- src/context/UserProvider.jsx
+++ src/context/UserProvider.jsx
@@ -1,12 +1,34 @@
-import { createContext, useState } from 'react'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth'
+import { createContext, useEffect, useState } from 'react'
+import { auth } from '../firebase'
 
 export const UserContext = createContext()
 
-const UserProvider = ({children}) => {
+const UserProvider = ({ children }) => {
   const [user, setUser] = useState(false)
 
-  return(
-    <UserContext.Provider value={{user, setUser}}>
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      console.log(user)
+      if (user) {
+        const { email, photoURL, displayName, uid } = user
+        setUser({ email, photoURL, displayName, uid })
+      } else {
+        setUser(null)
+      }
+    })
+    return () => unsubscribe()
+  }, [])
+
+  const registerUser = (email, password) => createUserWithEmailAndPassword(auth, email, password)
+
+  const loginUser = (email, password) => signInWithEmailAndPassword(auth, email, password)
+
+  const signOutUser = () => signOut(auth)
+
+
+  return (
+    <UserContext.Provider value={{ user, setUser, registerUser, loginUser, signOutUser }}>
       {children}
     </UserContext.Provider>
   )
