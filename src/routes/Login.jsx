8b3d20f48cbb825743f15903f3a98f3ae7acacac
abrojodigital <abrojodigital@gmail.com>
2022-07-25 11:41:51 -0300

    primer commit

---
+++ src/routes/Login.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react"
+import { UserContext } from "../context/UserProvider"
+import { useNavigate } from "react-router"
+
+const Login = () => {
+  const { user, setUser } = useContext(UserContext)
+  const navegate = useNavigate()
+  const handleClickLogin = () => {
+    setUser(true)
+    navegate("/")
+  }
+
+  return (
+    <>
+      <h1>Login</h1>
+      <h2>{user ? "En línea" : "Offline"}</h2>
+      <button onClick={handleClickLogin}>Acceder</button>
+    </>
+  )
+}
+export default Login
